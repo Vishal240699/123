90bf2fd16f07a7f3fdf91e0ffb63a08cf97edd67
Vishal Kumar <vishalkumar@Vishals-MacBook-Pro.local>
2024-08-20 05:11:21 -0700

    123

---
+++ core/llm/llms/index.ts
@@ -0,0 +1,133 @@
+import {
+  BaseCompletionOptions,
+  IdeSettings,
+  ILLM,
+  LLMOptions,
+  ModelDescription,
+} from "../..";
+import { renderTemplatedString } from "../../promptFiles/renderTemplatedString";
+import { DEFAULT_MAX_TOKENS } from "../constants";
+import { BaseLLM } from "../index";
+import Anthropic from "./Anthropic";
+import Azure from "./Azure";
+import Bedrock from "./Bedrock";
+import Cloudflare from "./Cloudflare";
+import Cohere from "./Cohere";
+import DeepInfra from "./DeepInfra";
+import Deepseek from "./Deepseek";
+import Fireworks from "./Fireworks";
+import Flowise from "./Flowise";
+import FreeTrial from "./FreeTrial";
+import Gemini from "./Gemini";
+import Groq from "./Groq";
+import HuggingFaceInferenceAPI from "./HuggingFaceInferenceAPI";
+import HuggingFaceTGI from "./HuggingFaceTGI";
+import LMStudio from "./LMStudio";
+import LlamaCpp from "./LlamaCpp";
+import Llamafile from "./Llamafile";
+import Mistral from "./Mistral";
+import Msty from "./Msty";
+import Ollama from "./Ollama";
+import OpenAI from "./OpenAI";
+import Replicate from "./Replicate";
+import SageMaker from "./SageMaker";
+import TextGenWebUI from "./TextGenWebUI";
+import Together from "./Together";
+import WatsonX from "./WatsonX";
+import ContinueProxy from "./stubs/ContinueProxy";
+
+const LLMs = [
+  Anthropic,
+  Cohere,
+  FreeTrial,
+  Gemini,
+  Llamafile,
+  Ollama,
+  Replicate,
+  TextGenWebUI,
+  Together,
+  HuggingFaceTGI,
+  HuggingFaceInferenceAPI,
+  LlamaCpp,
+  OpenAI,
+  LMStudio,
+  Mistral,
+  Bedrock,
+  SageMaker,
+  DeepInfra,
+  Flowise,
+  Groq,
+  Fireworks,
+  ContinueProxy,
+  Cloudflare,
+  Deepseek,
+  Msty,
+  Azure,
+  WatsonX,
+];
+
+export async function llmFromDescription(
+  desc: ModelDescription,
+  readFile: (filepath: string) => Promise<string>,
+  uniqueId: string,
+  ideSettings: IdeSettings,
+  writeLog: (log: string) => Promise<void>,
+  completionOptions?: BaseCompletionOptions,
+  systemMessage?: string,
+): Promise<BaseLLM | undefined> {
+  const cls = LLMs.find((llm) => llm.providerName === desc.provider);
+
+  if (!cls) {
+    return undefined;
+  }
+
+  const finalCompletionOptions = {
+    ...completionOptions,
+    ...desc.completionOptions,
+  };
+
+  systemMessage = desc.systemMessage ?? systemMessage;
+  if (systemMessage !== undefined) {
+    systemMessage = await renderTemplatedString(systemMessage, readFile, {});
+  }
+
+  let options: LLMOptions = {
+    ...desc,
+    completionOptions: {
+      ...finalCompletionOptions,
+      model: (desc.model || cls.defaultOptions?.model) ?? "codellama-7b",
+      maxTokens:
+        finalCompletionOptions.maxTokens ??
+        cls.defaultOptions?.completionOptions?.maxTokens ??
+        DEFAULT_MAX_TOKENS,
+    },
+    systemMessage,
+    writeLog,
+    uniqueId,
+  };
+
+  if (desc.provider === "continue-proxy") {
+    options.apiKey = ideSettings.userToken;
+    if (ideSettings.remoteConfigServerUrl) {
+      options.apiBase = new URL(
+        "/proxy/v1",
+        ideSettings.remoteConfigServerUrl,
+      ).toString();
+    }
+  }
+
+  return new cls(options);
+}
+
+export function llmFromProviderAndOptions(
+  providerName: string,
+  llmOptions: LLMOptions,
+): ILLM {
+  const cls = LLMs.find((llm) => llm.providerName === providerName);
+
+  if (!cls) {
+    throw new Error(`Unknown LLM provider type "${providerName}"`);
+  }
+
+  return new cls(llmOptions);
+}
